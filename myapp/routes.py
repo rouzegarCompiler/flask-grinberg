a947438b7c239744e8148f9cade1134155369582
SMR <mohammad.rouzegar@gmail.com>
2022-07-26 08:26:36 +0430

    Part 2.2

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,22 +1,9 @@
+from flask import render_template
 from myapp import app
+
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
-
-
-@app.route("/post")
-def post():
-    return """
-        <!DOCTYPE html>
-        <html>
-        <head>
-        <title>Post page</title>
-        </head>
-        <body>
-        <h1>Post page</h1>
-        <p>This is sample post</p>
-        </body>
-        </html>
-    """
+    myuser = {"username": "mohammad"}
+    return render_template("index.html",user=myuser,title="Home")
