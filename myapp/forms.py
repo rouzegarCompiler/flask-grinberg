28bd62e6502b8ff1693846063e28c17399668cd2
SMR <mohammad.rouzegar@gmail.com>
2022-07-27 15:02:48 +0430

    Part 4.8

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,BooleanField,SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
+
 class LoginForm(FlaskForm):
-    username = StringField(label='Username',validators=[DataRequired()])
-    password = PasswordField(label="Password",validators=[DataRequired()])
+    username = StringField(label='Username', validators=[DataRequired()])
+    password = PasswordField(label="Password", validators=[DataRequired()])
     remember_me = BooleanField(label="Remember Me")
     submit = SubmitField(label="Sign In")
--- myapp/models.py
+++ myapp/models.py
@@ -1,10 +1,23 @@
 from myapp import db
+from datetime import datetime
+
 
 class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(64),index=True,unique=True)
-    email = db.Column(db.String(128),index=True,unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=True)
     password = db.Column(db.String(128))
+    posts = db.relationship('Post', backref="author", lazy="dynamic")
 
     def __repr__(self):
         return f"<User {self.username} >"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text())
+    timestamp = db.Column(db.DateTime(), index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+    def __repr__(self):
+        return f"<Post {self.body}>"
