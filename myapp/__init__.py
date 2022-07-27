774507a2923ad5b6c31ef7ad9278bd4af0712814
SMR <mohammad.rouzegar@gmail.com>
2022-07-27 19:32:21 +0430

    Part 5.4

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
 app.config["SQLALCHEMY_ECHO"] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from myapp import routes, models
