be699cc8f8c9151b4d3fa277f2f26d2fe721e44a
SMR <mohammad.rouzegar@gmail.com>
2022-07-27 19:38:03 +0430

    Part 5.5

--- myapp/models.py
+++ myapp/models.py
@@ -1,4 +1,4 @@
-from myapp import db
+from myapp import db, login
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
@@ -19,6 +19,11 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login.load_user
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
