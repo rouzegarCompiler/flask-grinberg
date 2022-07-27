04ade850000d03067d2b6b92ec76a490235cb00d
SMR <mohammad.rouzegar@gmail.com>
2022-07-28 00:20:25 +0430

    Part 5.6

--- myapp/routes.py
+++ myapp/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template,redirect,flash,url_for
+from flask import render_template, redirect, flash, url_for, session
+from flask_login import login_user, current_user
 from myapp import app
 from myapp.forms import LoginForm
+from myapp.models import User
 
 
 @app.route("/")
@@ -9,23 +11,29 @@ def index():
     myuser = {"username": "mohammad"}
     myposts = [
         {
-            "author" : {"username" : "mohammd"},
-            "body" : "This is the first post ."
+            "author": {"username": "mohammd"},
+            "body": "This is the first post ."
         },
         {
-            "author" : {"username" : "zahra"},
-            "body" : "Avengers is the best movie i have ever seen !"
+            "author": {"username": "zahra"},
+            "body": "Avengers is the best movie i have ever seen !"
         }
     ]
 
-    return render_template("index.html",user=myuser,title="Home",posts=myposts)
+    return render_template("index.html", user=myuser, title="Home", posts=myposts)
 
-@app.route("/login", methods=["GET","POST"])
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+
     login_form = LoginForm()
     if login_form.validate_on_submit():
-        flash_message = f"Username {login_form.username.data} logged in and remember me value is {login_form.remember_me.data}"
-        flash(flash_message)
-        return redirect(url_for('index'))
-
-    return render_template('login.html',form=login_form)
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if user is None or not user.check_password(login_form.password.data):
+            flash("Invalid username or password !")
+            return redirect(url_for("login"))
+        login_user(user=user, remember=login_form.remember_me.data)
+        return redirect(url_for("index"))
+    return render_template('login.html', form=login_form)
