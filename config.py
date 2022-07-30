9e868e2ba097dd170e84e6b41331b946fdff851a
SMR <mohammad.rouzegar@gmail.com>
2022-07-30 14:22:50 +0430

    Part 7.3 and other changes

--- config.py
+++ config.py
@@ -4,13 +4,19 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    # FLASK
     FLASK_APP = os.environ.get("FLASK_APP") or "microblog.py"
 
     FLASK_ENV = os.environ.get("FLASK_ENV") or "development"
 
+    FLASK_DEBUG = int(os.environ.get("FLASK_DEBUG")) or 1
+
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
 
+    # FLASK_SQLALCHEMY
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
                               "sqlite:///" + os.path.join(basedir, "app.db")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = bool(os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")) or False
+
+    SQLALCHEMY_ECHO = bool(os.environ.get("SQLALCHEMY_ECHO")) or False
